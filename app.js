36752135f652cb35f7b4fe4dc8e5164e7b91b778
Ratul-Saha-003 <cs.ratul03@gmail.com>
2024-10-11 11:32:39 +0530

    update cors

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true })
   }
 ).catch(err => console.log(process.env.DB_CONNECT, "Connection Failed", err));
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://masterstack-chi.vercel.app', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false,limit: '10mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
